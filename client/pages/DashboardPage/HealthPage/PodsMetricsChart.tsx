31effb20b54bc44d6922b0cf513d83c128f9dafb
Aalayah-Olaes <108641773+AalayahOlaes@users.noreply.github.com>
2023-06-15 15:35:07 -0400

    test for pod metric retreival

---
+++ client/pages/DashboardPage/HealthPage/PodsMetricsChart.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useEffect, useState } from 'react';
+
+type MetricProps = {
+  label: string;
+}
+
+const PodsMetricsTable: FC<MetricProps> = (props) => {
+  const [metricDivs, setMetricDivs] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('/api/dashboard/count');
+        const data = await res.json();
+
+        const newMetricDivs: JSX.Element[] = [];
+        for (const prop in data) {
+          newMetricDivs.push(
+            <div className='metric-box'>
+              <div className='metric-label'>{prop}</div>
+              <div className='metric-value'>{data[prop]}</div>
+            </div>
+          )
+        }
+        setMetricDivs(newMetricDivs);
+      } catch (error) {
+        console.log('Error fetching data: ', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div id="metrics-table">
+      {metricDivs}
+    </div>
+  )
+}
+
+export default PodsMetricsTable;
