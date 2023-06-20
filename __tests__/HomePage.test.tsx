52b53fe8c19feb59adb1335f919016b060104281
pearlhchang <p.chang1802@gmail.com>
2023-06-19 18:23:52 -0700

    add rendering tests for home page

--- __tests__/HomePage.test.tsx
+++ __tests__/HomePage.test.tsx
@@ -1,4 +1,29 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
+
+import HomePage from '../client/pages/HomePage';
+
+describe('Home Page - Rendering', () => {
+  it ('should have "Login" text', () => {
+    render(
+      <Router>
+        <Routes>
+            <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Router>
+    );
+    expect(screen.getByText('Login')).toBeInTheDocument();
+  });
+  it ('should have "Register" text', () => {
+    render(
+      <Router>
+        <Routes>
+            <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Router>
+    );
+    expect(screen.getByText('Register')).toBeInTheDocument();
+  });
+});
