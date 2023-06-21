b331554ab8c2dad4df21a12ae033bb8e652e1167
pearlhchang <p.chang1802@gmail.com>
2023-06-21 08:25:40 -0700

    add content to home page
    Co-Authored-By: Aalayah-Olaes <108641773+aalayaholaes@users.noreply.github.com>

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -6,29 +6,77 @@ import RegisterPage from './RegisterPage';
 const HomePage = () => {
   return (
     <>
-      <nav className="flex justify-between bg-gray-800 h-20">
-        <div className="flex space-x-3 items-center text-lg">
-          <Link to="/" 
-          className="hover:underline"
-          >Overview</Link>
-          <Link to="/#features"
-          className="hover:underline"
-          >Features</Link>
-          <Link to="/#demo"
-          className="hover:underline"
-          >Demo</Link>
-          <Link to="/#get-started"
-          className="hover:underline"
-          >Get Started</Link>
-          <Link to="/#team"
-          className="hover:underline"
-          >Team</Link>
+      <nav className="flex h-24 justify-between">
+        <div className="flex items-center space-x-3 text-lg">
+          <img src="./assets/logo.png" className="h-24 w-24" />
+          <a href="/#overview" className="hover:underline">
+            Overview
+          </a>
+          <a href="/#features" className="hover:underline">
+            Features
+          </a>
+          <a href="/#get-started" className="hover:underline">
+            Get Started
+          </a>
+          <a href="/#demo" className="hover:underline">
+            Demo
+          </a>
+          <a href="/#team" className="hover:underline">
+            Team
+          </a>
         </div>
-        <div className="flex space-x-3 mr-10 items-center">
-          <Link to="/login" className="rounded border border-black bg-sky-600 hover:bg-sky-700 w-20 h-7 content-center pl-5" >Login</Link>
-          <Link to="/register" className="rounded border border-black bg-sky-600 hover:bg-sky-700 w-20 h-7 content-center pl-3">Register</Link>
+        <div className="mr-10 flex items-center space-x-3">
+          <Link to="/login" className="rounded bg-gradient-to-r from-cyan-500 to-blue-600 px-6 py-2 font-bold">
+            Login
+          </Link>
+          <Link to="/register" className="rounded bg-gradient-to-r from-cyan-500 to-blue-600 px-4 py-2 font-bold">
+            Register
+          </Link>
         </div>
       </nav>
+      <div id="#overview" className="border border-white">
+        <h1>Introducing Eisodos</h1>
+        <p>
+          Eisodos is a developer-friendly tool that allows users to easily montior and visualize key Kubernetes health
+          metrics and cluster metrics.
+        </p>
+        <a href="#features">Explore</a>
+      </div>
+      <div id="features" className="border border-white">
+        Features
+        <div>
+          <h3>Pod Metrics</h3>
+
+          <p>View container-related metrics on a dashboard, presented with visualizations for easy interpretation.</p>
+        </div>
+        <div>
+          <h3>Cluster Metrics</h3>
+
+          <p>
+            Monitor cluster-wide metrics with user-friendly graphs providing clear insights into the cluster's
+            performance.
+          </p>
+        </div>
+        <div>
+          <h3>Secure Authentication</h3>
+
+          <p>
+            Uphold trust in Eisodos by utilizing robust encryption and hashing techniques to ensure secure
+            authentication.
+          </p>
+        </div>
+        <div>
+          <h3>Node Hierarchy</h3>
+
+          <p>
+            Gain a comprehensive understanding of your cluster's structure by visualizing the hierarchical relationship
+            between nodes.
+          </p>
+        </div>
+      </div>
+      <div id="get-started" className="border border-white">Get Started</div>
+      <div id="demo" className="border border-white">Demo</div>
+      <div id="team" className="border border-white">Team</div>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
