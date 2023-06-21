5a2dc2191447b354c80f4ea50df8032d17a930df
Ron Liu <itsronliu@gmail.com>
2023-06-21 00:13:05 -0700

    Format HomePage.tsx with prettier

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -6,27 +6,35 @@ import RegisterPage from './RegisterPage';
 const HomePage = () => {
   return (
     <>
-      <nav className="flex justify-between bg-gray-800 h-20">
-        <div className="flex space-x-3 items-center text-lg">
-          <Link to="/" 
-          className="hover:underline"
-          >Overview</Link>
-          <Link to="/#features"
-          className="hover:underline"
-          >Features</Link>
-          <Link to="/#demo"
-          className="hover:underline"
-          >Demo</Link>
-          <Link to="/#get-started"
-          className="hover:underline"
-          >Get Started</Link>
-          <Link to="/#team"
-          className="hover:underline"
-          >Team</Link>
+      <nav className="flex h-20 justify-between bg-gray-800">
+        <div className="flex items-center space-x-3 text-lg">
+          <Link to="/" className="hover:underline">
+            Overview
+          </Link>
+          <Link to="/#features" className="hover:underline">
+            Features
+          </Link>
+          <Link to="/#demo" className="hover:underline">
+            Demo
+          </Link>
+          <Link to="/#get-started" className="hover:underline">
+            Get Started
+          </Link>
+          <Link to="/#team" className="hover:underline">
+            Team
+          </Link>
         </div>
-        <div className="flex space-x-3 mr-10 items-center">
-          <Link to="/login" className="rounded border border-black bg-sky-600 hover:bg-sky-700 w-20 h-7 content-center pl-5" >Login</Link>
-          <Link to="/register" className="rounded border border-black bg-sky-600 hover:bg-sky-700 w-20 h-7 content-center pl-3">Register</Link>
+        <div className="mr-10 flex items-center space-x-3">
+          <Link
+            to="/login"
+            className="h-7 w-20 content-center rounded border border-black bg-sky-600 pl-5 hover:bg-sky-700">
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="h-7 w-20 content-center rounded border border-black bg-sky-600 pl-3 hover:bg-sky-700">
+            Register
+          </Link>
         </div>
       </nav>
       <Routes>
