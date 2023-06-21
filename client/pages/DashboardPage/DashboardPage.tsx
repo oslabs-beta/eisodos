7dc39877ad247a13d8644d79adee74964474f390
Ron Liu <itsronliu@gmail.com>
2023-06-21 00:57:15 -0700

    Add comments for dashboard page

--- client/pages/DashboardPage/DashboardPage.tsx
+++ client/pages/DashboardPage/DashboardPage.tsx
@@ -23,13 +23,17 @@ const DashboardPage = () => {
 
   return (
     <div className="flex">
+      {/* sidebar */}
       <nav className="flex flex-col gap-y-8 px-6 pt-12">
+        {/* current cluster */}
+        {/* TODO: turn this into a dropdown */}
         <Menu as="div">
           <Menu.Button className="inline-flex w-48 items-center justify-between rounded-lg bg-white/10 px-4 py-3 shadow-sm hover:bg-white/20">
             my-cluster
             <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5 text-white-1" aria-hidden="true" />
           </Menu.Button>
         </Menu>
+        {/* links for different tabs */}
         <ul className="mx-3.5 flex flex-col gap-y-7 text-lg">
           {navigation.map((item) => (
             <li key={item.name}>
@@ -40,6 +44,7 @@ const DashboardPage = () => {
           ))}
         </ul>
       </nav>
+      {/* active tab */}
       <div>
         <Outlet />
       </div>
