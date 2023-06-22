4613f280a7f4ec638f5722f46ee0d31833ffae00
Ron Liu <itsronliu@gmail.com>
2023-06-21 23:40:25 -0700

    Update Health for new metrics response data

--- client/pages/DashboardPage/Health/Health.tsx
+++ client/pages/DashboardPage/Health/Health.tsx
@@ -1,55 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { DataPoint, DataObj } from './charts.types';
+
+// import chart components
+import CPUResponsiveBar from './charts/CPUBar';
+import PodsMetricsTable from './charts/PodsMetricsChart';
+
+// TODO: rename these charts
 import CPUUsageChart from './charts/CPUUsageChart';
 import MemChart from './charts/MemUsageChart';
+
+// TODO: add these charts to health tab
 import NetworkTransmitChart from './charts/NetworkTransmitChart';
 import NetworkReceiveChart from './charts/NetworkReceiveChart';
-import PodsMetricsTable from './charts/PodsMetricsChart';
-import CPUResponsiveBar from './charts/CPUBar';
+
+// types for fetched data
+interface Metrics {
+  cpu: Metric[];
+  memory: Metric[];
+}
+
+interface Metric {
+  timestamp: number;
+  value: string;
+}
 
 const Health = () => {
-  interface Metrics {
-    cpuValues: string[];
-    memValues: string[];
-    networkTransmitValues: string[];
-    networkReceiveValues: string[];
-  }
-
-  const [cpuData, setCpuData] = useState<DataObj[]>([{ id: 'cpuUsage', data: [] }]);
-  const [memData, setMemData] = useState<DataObj[]>([{ id: 'memUsage', data: [] }]);
-  const [networkTransmitData, setNetworkTransmitData] = useState<DataObj[]>([{ id: 'networkTransmitInfo', data: [] }]);
-  const [networkReceiveData, setNetworkReceiveData] = useState<DataObj[]>([{ id: 'networkReceiveInfo', data: [] }]);
-
-  async function getData(): Promise<void> {
-    const res = await fetch('/api/dashboard/metrics');
-    const metrics: Promise<Metrics> = res.json();
-
-    const cpuValues = (await metrics).cpuValues;
-    const memValues = (await metrics).memValues;
-    const networkTransmitValues = (await metrics).networkTransmitValues;
-    const networkReceiveValues = (await metrics).networkReceiveValues;
-
-    setCpuData(processChartData(cpuValues, 'cpuUsage'));
-    setMemData(processChartData(memValues, 'memUsage'));
-    setNetworkTransmitData(processChartData(networkTransmitValues, 'networkTransmitUsage'));
-    setNetworkReceiveData(processChartData(networkReceiveValues, 'networkReceiveUsage'));
-  }
-
-  function processChartData(values: string[], id: string): DataObj[] {
-    const dataPoints: DataPoint[] = [];
-    for (let i = 0; i < values.length; i++) {
-      const dataPoint = {
-        x: i,
-        y: parseFloat(values[i])
-      };
-      dataPoints.push(dataPoint);
-    }
-    return [{ id, data: dataPoints }];
-  }
+  const [cpuData, setCpuData] = useState<DataObj[]>([]);
+  const [memData, setMemData] = useState<DataObj[]>([]);
 
   useEffect(() => {
     getData();
   }, []);
+
+  async function getData(): Promise<void> {
+    const res = await axios.get('/api/dashboard/global-metrics?time=10m');
+    const metrics: Metrics = res.data;
+
+    // format metrics for nivo charts
+    setCpuData(processChartData(metrics.cpu, 'cpuUsage'));
+    setMemData(processChartData(metrics.memory, 'memUsage'));
+  }
+
+  function processChartData(metrics: Metric[], id: string): DataObj[] {
+    const dataPoints: DataPoint[] = [];
+    for (let i = 0; i < metrics.length; i++) {
+      const dataPoint = {
+        x: metrics[i].timestamp,
+        y: parseFloat(metrics[i].value)
+      };
+      dataPoints.push(dataPoint);
+    }
+    console.log(dataPoints);
+    return [{ id, data: dataPoints }];
+  }
 
   return (
     <div>
@@ -64,12 +68,6 @@ const Health = () => {
         </div>
         <div style={{ flexBasis: '50%' }}>
           <MemChart chartData={memData} />
-        </div>
-        <div style={{ flexBasis: '50%' }}>
-          <NetworkTransmitChart chartData={networkTransmitData} />
-        </div>
-        <div style={{ flexBasis: '50%' }}>
-          <NetworkReceiveChart chartData={networkReceiveData} />
         </div>
       </div>
     </div>
