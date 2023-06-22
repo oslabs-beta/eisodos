40e284a7c9be956c2719c2dcd84808be00a85213
pearlhchang <p.chang1802@gmail.com>
2023-06-22 01:42:54 -0700

    update legend

--- client/pages/Legend.tsx
+++ client/pages/Legend.tsx
@@ -6,7 +6,7 @@ const Legend: React.FC = () => {
     <div>
       <div className="absolute right-0 top-0 w-96 rounded-l-lg rounded-br-lg bg-black-2 text-white">
         {/* <h4 className="mb-2 mt-3 text-center text-lg font-bold">Legend</h4> */}
-        <div className="my-3 flex justify-around">
+        <div className="my-3 flex justify-evenly">
           <div className="flex items-center">
             <svg width="20" height="20" className="mr-1">
               <rect width="20" height="20" style={{ fill: '#2563eb' }} />
